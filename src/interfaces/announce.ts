d18e7c118d157f03d7d2735af2464cdd89235d92
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-04-24 12:31:06 -0300

    feat: updated serializers

--- src/interfaces/announce.ts
+++ src/interfaces/announce.ts
@@ -9,7 +9,13 @@ export interface IImagemRequest{
   image1?:string;
   image2?:string;
   image3?:string
+}
 
+export interface IImagemResponse{
+  id: string;
+  image1?:string;
+  image2?:string;
+  image3?:string
 }
 
 
@@ -40,6 +46,24 @@ export interface IAnnounceUpdate{
 
 }  
 
+export interface IAnnounceCreateResponce{
+  id:string
+  kilometer:string;
+  price_FIPE:string;
+  price:string;
+  description:string;
+  image_cover:string;
+  published:boolean;
+  tag:boolean;
+  brand:IBrandResponce;
+  fuel:IFuelResponce;
+  color:IColorResponce;
+  year:IYearResponce;
+  model:IModelResponce;
+  image:IImagemResponse;
+  user:IUserResponseWithoutPassword;
+}
+
 export interface IAnnounceResponce{
   id:string
   kilometer:string;
@@ -54,6 +78,5 @@ export interface IAnnounceResponce{
   color:IColorResponce;
   year:IYearResponce;
   model:IModelResponce;
-  image:IImagemRequest;
-  user:IUserResponseWithoutPassword;
-}  
+  image:IImagemResponse;
+}
--- src/serializers/announce.serializer.ts
+++ src/serializers/announce.serializer.ts
@@ -1,3 +1,91 @@
 import * as yup from "yup";
+import { IAnnounceCreateResponce, IAnnounceResponce } from "../interfaces/announce";
 
-// export const announceSerializer:
+export const announceSerializer: yup.ObjectSchema<IAnnounceResponce> = yup
+.object()
+.shape({
+    id: yup.string().required(),
+    kilometer: yup.string().required(),
+    price_FIPE: yup.string().required(),
+    price: yup.string().required(),
+    description: yup.string().required(),
+    image_cover: yup.string().required(),
+    published: yup.boolean().required(),
+    tag: yup.boolean().required(),
+    brand: yup.object().shape({
+        id: yup.string().required(),
+        brand: yup.string().required(),
+    }),
+    fuel: yup.object().shape({
+        id: yup.string().required(),
+        fuel: yup.string().required(),
+    }),
+    color: yup.object().shape({
+        id: yup.string().required(),
+        color: yup.string().required(),
+    }),
+    year: yup.object().shape({
+        id: yup.string().required(),
+        year: yup.string().required(),
+    }),
+    model: yup.object().shape({
+        id: yup.string().required(),
+        model: yup.string().required(),
+    }),
+    image: yup.object().shape({
+        image1: yup.string().notRequired(),
+        image2: yup.string().notRequired(),
+        image3: yup.string().notRequired(),
+        id: yup.string().required()
+    }),
+})
+
+export const announceCreateSerializer: yup.ObjectSchema<IAnnounceCreateResponce> = yup
+.object()
+.shape({
+    id: yup.string().required(),
+    kilometer: yup.string().required(),
+    price_FIPE: yup.string().required(),
+    price: yup.string().required(),
+    description: yup.string().required(),
+    image_cover: yup.string().required(),
+    published: yup.boolean().required(),
+    tag: yup.boolean().required(),
+    brand: yup.object().shape({
+        id: yup.string().required(),
+        brand: yup.string().required(),
+    }),
+    fuel: yup.object().shape({
+        id: yup.string().required(),
+        fuel: yup.string().required(),
+    }),
+    color: yup.object().shape({
+        id: yup.string().required(),
+        color: yup.string().required(),
+    }),
+    year: yup.object().shape({
+        id: yup.string().required(),
+        year: yup.string().required(),
+    }),
+    model: yup.object().shape({
+        id: yup.string().required(),
+        model: yup.string().required(),
+    }),
+    image: yup.object().shape({
+        image1: yup.string().notRequired(),
+        image2: yup.string().notRequired(),
+        image3: yup.string().notRequired(),
+        id: yup.string().required()
+    }),
+    user: yup.object().shape({
+        id: yup.string().required(),
+        name: yup.string().required(),
+        email: yup.string().email().required(),
+        cpf: yup.string().required(),
+        cellphone: yup.string().required(),
+        date_birth: yup.string().required(),
+        description: yup.string().required(),
+        account_type: yup.boolean().required(),
+        announce: yup.array(announceSerializer)
+    })
+})
