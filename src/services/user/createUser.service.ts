099a9cbb61a78f33c16cbaa4bb4bd7d9c5224e8c
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-04-27 13:25:26 -0300

    fix: edit coments user address

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -1,36 +1,9 @@
-// import { IUserRequest } from "../../interfaces/user";
-// import { AppDataSource } from "../../data-source";
-// import { User } from "../../entities/user";
-// import AppError from "../../errors/AppError";
-// import { userWithoutPasswordSerializer } from "../../serializers/user.serializer";
-
-// const createUserService = async (userData: IUserRequest) => {
-//   const { email } = userData;
-//   const userRep = AppDataSource.getRepository(User);
-//   const emailUser = await userRep.findOneBy({ email: email });
-//   if (emailUser) {
-//     throw new AppError("Email as already registered", 400);
-//   }
-//   const user = userRep.create(userData);
-
-//   await userRep.save(user);
-
-//   const userWithoutPassword = await userWithoutPasswordSerializer.validate(
-//     user,
-//     { stripUnknown: true }
-//   );
-
-//   return userWithoutPassword;
-// };
-// export default createUserService;
-
 import { IUserRequest } from "../../interfaces/user";
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user";
 import AppError from "../../errors/AppError";
 import { userWithoutPasswordSerializer } from "../../serializers/user.serializer";
 import { Address } from "../../entities/address";
-import { IaddressResponce } from "../../interfaces/address";
 import { Repository } from "typeorm";
 
 const createUserService = async (userData: IUserRequest) => {
@@ -62,7 +35,6 @@ const createUserService = async (userData: IUserRequest) => {
       stripUnknown: true,
     }
   );
-  console.log(userWithoutPassword);
 
   return userWithoutPassword;
 };
